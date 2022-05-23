cd6ae3ba5cc9ff59cc126d47b10316a32145c47c
10152504 <seungwoo.ham@kt.com>
2022-05-23 10:40:08 +0900

    create button

---
+++ src/views/project/CreateButton.vue
@@ -0,0 +1,91 @@
+<template>
+    <!-- block 안의 inline만 text-align 적용 가능 -->
+    <table v-for="project in projects" :key="project.name">
+        <div class="proj_name">
+            <span>{{ project.name }}</span>
+        </div>
+        <tr>
+            <th>Data Group</th>
+            <th>Description</th>
+            <th>생성하기</th>
+        </tr>
+        <tr v-for="group in project.dataGroup" :key="group.name">
+            <!--td><a target="_blank" :href="item.link">{{item.link}}</a></td>
+            <td><img v-bind:src="item.image"></td-->
+            <td><span>{{ group.name }}</span></td>
+            <td><span>{{ group.description }}</span></td>
+            <td><button class="edit_btn">Edit</button></td>
+        </tr>
+    </table>
+</template>
+<script>
+import { ref } from "vue";
+import axios from "axios";
+import { useStore } from "vuex";
+
+export default {
+    name: 'ProjectList',
+    setup() {
+        const store = useStore();
+        let projects = ref([]);
+
+        function createProject() {
+            async () => {
+                // spinner 로딩 시작
+                store.commit("startLoading");
+                await axios.get("https://dev-faker-be.herokuapp.com/projects", {
+                    headers: {
+                        "accept": 'application/json',
+                        "Content-Type": 'application/json',
+                        "Access-Control-Allow-Origin" : '*',
+                        'Authentication': 'Bearer ' + store.getters.getToken
+                    }
+                })
+                .then(res => {
+                    if (res.status === 200) {
+                        let data = res.data;
+                        console.log(data);
+                        projects = res.data;
+                    }
+                });
+                // spinner 로딩 중지
+                store.commit("stopLoading");
+            };
+        }
+
+        return { 
+            projects,
+            createProject
+        }
+    }
+}
+</script>
+
+<style>
+table {
+    width: 100%;
+    padding: 20px;
+}
+th {
+    background-color: #F5EEFA;
+}
+td, th {
+    padding: 10px;
+    border: 0.5px solid #D4B2D9;
+}
+body {
+    padding: 1rem;
+}
+.proj_name {
+    font-size: 25px;
+    text-align: left;
+    margin-top: 20px;
+}
+.edit_btn {
+    background-color: #E1E6F2;
+    padding: 5px;
+    border: 0.5px solid #E1ADF2;
+    border-radius: 10px;
+}
+
+</style>
--- src/views/project/ProjectList.vue
+++ src/views/project/ProjectList.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="mt-10">
         <!-- block 안의 inline만 text-align 적용 가능 -->
+        <button class="create_btn" id="create_button" v-on:click="btn_create"
+        @click="createNewProject(proj.projectNamespace)">Create</button>
+
         <div>
             <table v-for="proj in projects" :key="proj.projectId" class="mt-5">
                 <div class="top">
@@ -8,7 +11,7 @@
                     <button><font-awesome-icon 
                         icon="file-lines"
                         class="info_btn" 
-                        @click="showProjectInfo(proj.projectNamespace)"
+                        @click="f(proj.projectNamespace)"
                     /></button>
                 </div>
                 <tr>
@@ -28,6 +31,7 @@
         </div>
     </div>
     <ProjectInfoModal ref="project_info_modal" />
+    <ProjectCreateModal ref="project_create_modal" />
     <div v-if="isLoading" class="loading-container">
         <div class="loading">
             <FadeLoader />
@@ -50,6 +54,7 @@ export default {
         const store = useStore();
         let projects = ref([]);
         const project_info_modal = ref(null);
+        const project_create_modal = ref(null);
         let isLoading = computed(() => store.getters.getLoading);
         // 프로젝트 namespace로 정보 API 호출
         let namespace = ref("");
@@ -85,6 +90,19 @@ export default {
             // spinner 로딩 중지
             store.commit("stopLoading");
         })
+
+        const createNewProject = async (projectNamespace) => {
+            window.alert("hi");
+            store.commit("createProjectNamespace", projectNamespace);
+            const ok = await project_create_modal.value.show();
+            if (ok) { 
+                console.log("project 생성 완료");
+                // Refresh page
+                showProjects();
+            } else {
+                // result.value = "로그인 취소";
+            }
+        };
         
         onMounted(() => {
             showProjects();
@@ -95,7 +113,9 @@ export default {
             FadeLoader,
             isLoading,
             showProjectInfo,
+            createNewProject,
             project_info_modal,
+            project_create_modal,
             namespace
         }
     }
@@ -137,5 +157,11 @@ body {
     left: 50%;
     transform: translate(-50%, -50%);
     box-shadow: rgba(0, 0, 0, 0.1) 0 0 0 9999px;
+}
+.create_btn {
+    background-color: #E1E6F2;
+    padding: 5px;
+    border: 0.5px solid #E1ADF2;
+    border-radius: 10px;
 }
 </style>
